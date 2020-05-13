e0f365b4b451b375511d7221b69dae13b3ec33cb
Ravula Nishanth <nishanth.ravula@outlook.com>
2020-05-14 03:20:03 +0530

    Initial commit
    Working alright

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker/CountryPicker';
+
+import {Cards ,Chart ,CountryPicker} from './components'
+import styles from './App.module.css';
+import { fetchData } from './api';
+
+class App extends React.Component {
+    state = {
+        data: {},
+        country:'',
+    }
+
+    async componentDidMount () {
+        const fetchedData = await fetchData();
+
+        this.setState({ data: fetchedData });
+    }
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+
+        this.setState({ data: fetchedData, country: country });
+        // console.log(fetchedData);
+    }
+    render() {
+        const { data, country } = this.state;
+        return (
+            <div className={styles.container}>
+                <CountryPicker handleCountryChange = {this.handleCountryChange}/>
+                <Cards data = {data}/>
+                <Chart data = {data} country ={country}/> 
+            </div>
+        );
+    }
+}
+
+export default App;
